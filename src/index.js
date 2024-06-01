edb049a63527a85b31c8bfcb370bba78d4d4002b
Oyeins-GUI <oyeinx7@gmail.com>
2024-06-01 14:56:41 +0100

    inital commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const { config } = require("dotenv");
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+config();
+
+app.use(
+   cors({
+      origin: "*",
+   })
+);
+
+app.get("/api", async (req, res) => {
+   const response = await fetch(
+      "https://satscreener-api.up.railway.app/api/v1/stx20/rizz",
+      {
+         method: "GET",
+         headers: {
+            "api-key": process.env.RIZZ_API_KEY,
+         },
+      }
+   );
+   const data = await response.json();
+
+   res.status(200).json(data);
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
